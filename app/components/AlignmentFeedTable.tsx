d2f94fceed3e6aee75f6307a7c62dd1d73242b09
jbeshir <john@beshir.org>
2024-09-02 00:32:31 +0100

    Table: Add filter support

--- app/components/AlignmentFeedTable.tsx
+++ app/components/AlignmentFeedTable.tsx
@@ -45,21 +45,42 @@ function AlignmentFeedTable({apiBaseURL} : AlignmentFeedTableProps) {
 
     const columnDefs = [
         { flex: 3, colId: 'title', field: 'title', sortable: false,
+            filter: 'agTextColumnFilter',
+            filterParams: {
+                filterOptions: ['contains'],
+                maxNumConditions: 1
+            },
             cellRenderer: MakeLinkCellRenderer(useCallback((props: any) => {
                 return props.value || "";
             }, []))
         },
         { flex: 2, colId: 'authors', field: 'authors', sortable: false,
+            filter: 'agTextColumnFilter',
+            filterParams: {
+                filterOptions: ['contains'],
+                maxNumConditions: 1
+            },
             cellRenderer: MakeLinkCellRenderer(useCallback((props: any) => {
                 return props.value || "";
             }, []))
         },
         { flex: 1, colId: 'source', field: 'source',
+            filter: 'agTextColumnFilter',
+            filterParams: {
+                filterOptions: ['equals'],
+                maxNumConditions: 1
+            },
             cellRenderer: MakeLinkCellRenderer(useCallback((props: any) => {
                 return props.value || "";
             }, []))
         },
         { flex: 1, colId: 'published_at', field: 'published_at', headerName: 'Published At',
+            filter: 'agDateColumnFilter',
+            filterParams: {
+                defaultOption: 'greaterThanOrEqual',
+                filterOptions: ['inRange', 'lessThanOrEqual', 'greaterThanOrEqual'],
+                maxNumConditions: 1
+            },
             cellRenderer: MakeLinkCellRenderer(useCallback((props: any) => {
                 return props.value?.toLocaleString() || "";
             }, []))
@@ -80,6 +101,31 @@ function AlignmentFeedTable({apiBaseURL} : AlignmentFeedTableProps) {
             }).join(',')
             if (sort !== "") {
                 apiParams.set('sort', sort.toString());
+            }
+
+            const titleFilter = params.filterModel?.title?.filter;
+            if (titleFilter) {
+                apiParams.set('filter_title_fulltext', titleFilter);
+            }
+
+            const authorsFilter = params.filterModel?.authors?.filter;
+            if (authorsFilter) {
+                apiParams.set('filter_authors_fulltext', authorsFilter);
+            }
+
+            const publishedAfterFilter = params.filterModel?.published_at?.dateFrom;
+            if (publishedAfterFilter) {
+                apiParams.set('filter_published_after', (new Date(publishedAfterFilter)).toISOString());
+            }
+
+            const publishedBeforeFilter = params.filterModel?.published_at?.dateTo;
+            if (publishedBeforeFilter) {
+                apiParams.set('filter_published_before', (new Date(publishedBeforeFilter)).toISOString());
+            }
+
+            const sourceFilter = params.filterModel?.source?.filter;
+            if (sourceFilter) {
+                apiParams.set('filter_sources_allowlist', sourceFilter);
             }
 
             const apiURL = `${apiBaseURL}/v1/articles?${apiParams.toString()}`
