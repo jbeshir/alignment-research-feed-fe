9266be4d96dab7b42b7618d00c7a4fb80832b2ac
jbeshir <john@beshir.org>
2024-10-26 23:30:27 +0100

    Top Bar: Text in top left should link to root.

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -1,8 +1,9 @@
 import LoginLogoutButton from "~/components/LoginLogoutButton";
+import {Link} from "@remix-run/react";
 
 export default function TopBar() {
     return <div className='flex flex-row items-center bg-slate-50 dark:bg-slate-950'>
-        <h1 className='inline-block grow p-5 text-5xl text-left font-medium text-black dark:text-white'>Alignment Feed</h1>
+        <h1 className='inline-block grow p-5 text-5xl text-left font-medium text-black dark:text-white'><Link to="/">Alignment Feed</Link></h1>
         <div className='inline-block p-5 text-right font-medium text-black dark:text-white'>
             <LoginLogoutButton />
         </div>
