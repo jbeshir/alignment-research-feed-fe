e9fa88179c055eec11e58e96a48e92234b92af1b
jbeshir <john@beshir.org>
2024-10-28 01:17:14 +0000

    Favicon: Replace default favicon

--- app/root.tsx
+++ app/root.tsx
@@ -12,17 +12,22 @@ import type {LoaderFunction} from "@remix-run/cloudflare";
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body className='bg-slate-100 dark:bg-slate-800'>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
+    <head>
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96"/>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+        <link rel="shortcut icon" href="/favicon.ico"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <Meta/>
+        <Links/>
+    </head>
+    <body className='bg-slate-100 dark:bg-slate-800'>
+    {children}
+    <ScrollRestoration/>
+    <Scripts/>
+    </body>
     </html>
   );
 }
@@ -51,7 +56,7 @@ export default function App() {
           redirect_uri: auth0DefaultRedirectUri
       }}
       cacheLocation="localstorage"
-      useRefreshTokens="true"
+      useRefreshTokens=true
   >
       <Outlet />
   </Auth0Provider>;
