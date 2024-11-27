fd204efd6397c0b6a84f87f051683e82a7fa7756
Evan Harris <echarris@smcm.edu>
2024-11-27 13:06:16 -0500

    Use local string

--- app/components/ArticleInfo.tsx
+++ app/components/ArticleInfo.tsx
@@ -24,7 +24,7 @@ function ArticleInfo({article}: ArticleInfoProps) {
         </div>
         <div className="h-fill py-2 px-4 flex flex-row bg-slate-200 dark:bg-slate-700">
             <span className="inline-block w-40 font-bold">Published At</span>
-            <span className="inline-block grow">{article.published_at.toISOString()}</span>
+            <span className="inline-block grow">{article.published_at.toLocaleString()}</span>
         </div>
     </div>
 }
