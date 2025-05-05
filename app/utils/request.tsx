3b14aec302ec969c1d2380769e7ef4658fe5ef06
jbeshir <john@beshir.org>
2025-05-05 01:57:11 +0100

    Auth0: Automatically logout if we are unable to fetch an access token

--- app/utils/request.tsx
+++ app/utils/request.tsx
@@ -1,15 +1,20 @@
 import {Auth0ContextInterface} from "@auth0/auth0-react";
 
 export async function AuthenticatedFetch(req : Request, auth0Context: Auth0ContextInterface) : Promise<Response|null> {
-    const { isLoading, isAuthenticated, getAccessTokenSilently } = auth0Context;
+    const { isLoading, isAuthenticated, getAccessTokenSilently, logout } = auth0Context;
     if (isLoading) {
         return null;
     }
 
     if (isAuthenticated) {
-        const accessToken = await getAccessTokenSilently();
-        if (accessToken) {
-            req.headers.set('Authorization', `Bearer auth0|${accessToken}`);
+        try {
+            const accessToken = await getAccessTokenSilently();
+            if (accessToken) {
+                req.headers.set('Authorization', `Bearer auth0|${accessToken}`);
+            }
+        } catch (e) {
+            await logout();
+            return null;
         }
     }
 
