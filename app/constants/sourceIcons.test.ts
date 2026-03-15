bdce00dbb6445a52cda916679e5c1416a7587562
jbeshir <john@beshir.org>
2026-03-15 21:59:19 +0000

    Prettier: Fix formatting issue

--- app/constants/sourceIcons.test.ts
+++ app/constants/sourceIcons.test.ts
@@ -1,8 +1,5 @@
 import { describe, it, expect } from "vitest";
-import {
-  isVideoSource,
-  getSourcePlaceholderIcon,
-} from "./sourceIcons";
+import { isVideoSource, getSourcePlaceholderIcon } from "./sourceIcons";
 import {
   PlayIcon,
   ChatBubbleIcon,
